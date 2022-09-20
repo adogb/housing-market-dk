f9c450132b2d54e2d1c2dd3c44fa755b4f0597b5
adogb <13393572+adogb@users.noreply.github.com>
2022-09-20 21:54:37 +0200

    Add first draft of steps in pseudocode

---
+++ main.py
@@ -0,0 +1,12 @@
+# Fetch current HTML content from website (day 1)
+# Extract relevant data from each listing
+# Clean the data
+# Create a database
+# Each day
+#   Extract new offers on the website
+#   Clean
+#   Add to database
+#   Categorise housing to sold/for sale
+#     Compare list to sold housing page? Or Look for listings that are not there
+#     anymore?
+
