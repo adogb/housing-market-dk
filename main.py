5f6d976aa79df7745a9dc2dce90c701c53c751ab
adogb <13393572+adogb@users.noreply.github.com>
2022-09-28 18:36:06 +0200

    Refine pseudocode

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # Fetch current HTML content from website (day 1)
-# Extract relevant data from each listing
+# Extract relevant data from each listing and put in a dataframe
 # Clean the data
-# Create a database
+# Create a database MySQL / or CSV file?
 # Each day
-#   Extract new offers on the website
-#   Clean
-#   Add to database
-#   Categorise housing to sold/for sale
+#   Fetch new offers on the website
+#   Extract relevant data from each listing and put in database
+#   Categorise all housing to sold/for sale
 #     Compare list to sold housing page? Or Look for listings that are not there
 #     anymore?
 
