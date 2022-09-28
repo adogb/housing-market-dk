b306323c3d8bf129a179f0963299f11efa6879d8
adogb <13393572+adogb@users.noreply.github.com>
2022-09-28 22:46:05 +0200

    Search for relevant listings

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from operator import is_
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -9,8 +10,15 @@ import datetime
 url = 'https://www.boliga.dk/resultat?zipCodes=2720'
 r = requests.get(url)
 content = r.content
-soup = BeautifulSoup(content)
+soup = BeautifulSoup(content, "lxml")
 
+def is_relevant_listing(tag):
+  # find names of tag parents by mapping
+  parentsNames = list(map((lambda x: x.name), tag.parents))
+  return tag.name == "app-housing-list-item" and not "ngb-carousel" in parentsNames
+#tag_list=soup.find("app-housing-list-results").div.div.next_sibling.find_all("app-housing-list-item")
+tag_list=soup.find_all(is_relevant_listing)
+print(tag.a for tag in tag_list)
 
 # Fetch current HTML content from website (day 1) using Requests module
 # Extract (scrape) relevant data from each listing with BeautifulSoup and put in a dataframe
