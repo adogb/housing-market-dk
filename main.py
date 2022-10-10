8cc6720f4eb108ed9ffb954a32a52db7b9e30bac
adogb <13393572+adogb@users.noreply.github.com>
2022-10-10 09:54:56 +0200

    Use pandas str.replace correctly with regex

--- main.py
+++ main.py
@@ -94,16 +94,16 @@ df = df.apply(lambda col: col.str.strip())
 df.address = df.address.str.rstrip(",")
 df.energy_label = df.energy_label.str.replace("Energimærke: ", "")
 # %% cleaning numeric values
-df.price = df.price.str.replace(" kr.","").str.replace(".","")
-df.price_per_m2 = df.price_per_m2.str.replace(" kr. / m²","")\
-                                  .str.replace(".","")
+df.price = df.price.str.replace(" kr\.","").str.replace("\.","")
+df.price_per_m2 = df.price_per_m2.str.replace(" kr\. \/ m²","")\
+                                  .str.replace("\.","")
 df.area = df.area.str.replace(" m²","")
-df.monthly_cost = df.monthly_cost.str.replace(" kr. / md.","")\
+df.monthly_cost = df.monthly_cost.str.replace(" kr\. \/ md\.","")\
                                 .str.replace("Ejerudgift: ","")\
                                 .str.replace("Boligydelse: ","")\
-                                .str.replace(".","")
+                                .str.replace("\.","")
 df.rooms = df.rooms.str.replace("Værelser: ","").str.replace(" værelse","")
-df.ground_area = df.ground_area.str.replace(" m²","").str.replace(".","")
+df.ground_area = df.ground_area.str.replace(" m²","").str.replace("\.","")
 
 numeric_columns = ["price","price_per_m2","area","rooms", "ground_area", 
                    "year_built", "monthly_cost"]
@@ -126,7 +126,7 @@ df["date_added"] = df["date_added"].astype("datetime64[ns]")
 df["retrieved"] = df["retrieved"].astype("datetime64[ns]")
 
 # %% Create a database MySQL / or CSV file?
-df.to_csv("20221008_listings", index=False)
+# df.to_csv("20221008_listings.csv", index=False)
 
 # Each day
 #   Fetch new offers on the website
